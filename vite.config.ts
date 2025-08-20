cee37ac9331e522124302ac7d9e75cacd1ffa1bd
ZiyaZhang <ziyazhang@example.com>
2025-08-20 19:49:17 +0800

    Fix vite base path for local development

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/user-voice-system/",
+  base: "/",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
